18ac301330623e828e5d469a0e708c02e4d7bde8
tayyabaumtlhr <tayyabatayyaba2277@gmail.com>
2025-07-13 15:21:18 +0500

    the issue of rock paper scissors game has been resolved

--- rock_paper_scissor_merge.py
+++ rock_paper_scissor_merge.py
@@ -1,7 +1,31 @@
 # rps_gui.py
 
+import random
 import tkinter as tk
-from  rock_paper_scissor_backened import get_computer_choice, get_winner
+
+# --- Backend Functions ---
+
+# List of choices
+CHOICES = ['Rock', 'Paper', 'Scissors']
+
+# Determines the winner
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == 'Rock' and computer_choice == 'Scissors') or
+        (user_choice == 'Paper' and computer_choice == 'Rock') or
+        (user_choice == 'Scissors' and computer_choice == 'Paper')
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+# Gets a random computer choice
+def get_computer_choice():
+    return random.choice(CHOICES)
+
+# --- GUI (Frontend) ---
 
 # Main game window
 root = tk.Tk()
@@ -33,26 +57,3 @@ scissors_button.grid(row=0, column=2, padx=5)
 
 # Run the GUI loop
 root.mainloop()
-# rps_backend.py
-
-import random
-
-# List of choices
-CHOICES = ['Rock', 'Paper', 'Scissors']
-
-# Determines the winner
-def get_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "It's a tie!"
-    elif (
-        (user_choice == 'Rock' and computer_choice == 'Scissors') or
-        (user_choice == 'Paper' and computer_choice == 'Rock') or
-        (user_choice == 'Scissors' and computer_choice == 'Paper')
-    ):
-        return "You win!"
-    else:
-        return "Computer wins!"
-
-# Gets a random computer choice
-def get_computer_choice():
-    return random.choice(CHOICES)
